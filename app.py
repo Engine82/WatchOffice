15653c96529833428e4220271241b14643c6400a
Daniel <danmrtn3@gmail.com>
2023-10-23 15:55:48 -0400

    navbar & admin routes

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
 # HIRING
 # Hiring form / completed hiring
 @app.route("/hiring", methods=["GET", "POST"])
-def hire():
+def hiring():
     # After hiring is submitted
     if request.method == "POST":
         return render_template("hired.html")
@@ -29,3 +29,41 @@ def hire():
     # If starting new hiring
     else:
         return render_template("hiring.html")
+
+
+# SETTINGS
+# Add member
+@app.route("/add_member", methods=["GET", "POST"])
+def add_member():
+    # Add member to db
+    if request.method == "POST":
+        return render_template("added.html")
+
+    # Blank add member form
+    else:
+        return render_template("add.html")
+
+
+# Remove member
+@app.route("/remove_member", methods=["GET", "POST"])
+def remove_member():
+    # Remove member from active status
+    if request.method == "POST":
+        return render_template("removed.html")
+
+    # Blank removal form
+    else:
+        return render_template("remove.html")
+
+
+# Change member
+@app.route("/change_member", methods=["GET", "POST"])
+def change_member():
+    # Submit member changes
+    if request.method == "POST":
+        return render_template("changed.html")
+
+    # Blank member change form
+    else:
+        return render_template("change.html")
+
