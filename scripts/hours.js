37ac798b4633ecdcc72f2f7f71b5801698d45f10
Daniel <danmrtn3@gmail.com>
2023-11-15 18:08:17 -0500

    hours.js creted

---
+++ scripts/hours.js
@@ -0,0 +1,6 @@
+function showHours() {
+    if
+    document.getElementsByClassName("hours_1").style.display = "none";
+    else
+    document.getElementsByClassName("hours_2").style.display = "block";
+}
