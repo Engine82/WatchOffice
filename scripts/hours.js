d04a578723ee4a0dac107d44332936d60389254e
Dan Martin <danmrtn3@gmail.com>
2023-11-16 17:07:01 -0500

    ugh. I'll come back to this later

--- scripts/hours.js
+++ scripts/hours.js
@@ -1,6 +1,30 @@
-function showHours() {
-    if
-    document.getElementsByClassName("hours_1").style.display = "none";
-    else
-    document.getElementsByClassName("hours_2").style.display = "block";
-}
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').addEventListener('change', function() {
+        if (document.querySelector('#hours_1').) {
+        document.getElementsByClassName("hours_1").style.display = "none";
+        }
+        else {
+        document.getElementsByClassName("hours_1").style.display = "block";
+        }
+    });
+});
+
+
+const selectElement = document.querySelector('.day');
+const choice = document.querySelector('#hours_input_1');
+
+selectElement.addEventListener("change", (event) => {
+    hours_input_1.
+});  */
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementsByClassName('day').addEventListener('change', function() {
+        if (document.getElementsByClassName('#hours_1') == 'hours') {
+        document.getElementsByClassName("hours_1").style.display = "none";
+        }
+        else {
+        document.getElementsByClassName("hours_1").style.display = "block";
+        }
+    });
+});
