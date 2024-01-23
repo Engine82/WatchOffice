7278fddbbf94618fdf4517e00e64cca038897707
Daniel <danmrtn3@gmail.com>
2024-01-22 22:35:04 -0500

    Print button added to history function

--- static/print.js
+++ static/print.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     document.querySelector("#print").addEventListener('click', function(event) {
-//        event.preventDefault();
         printDiv("printableArea");
     });
 });
