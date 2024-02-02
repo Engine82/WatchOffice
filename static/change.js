af8437b56bdf96ed11fd1d2efa832b034b513677
Daniel <danmrtn3@gmail.com>
2024-02-01 21:13:42 -0500

    fix "no changes" display issue, get select value

--- static/change.js
+++ static/change.js
@@ -7,9 +7,10 @@ const password2 = document.getElementById("confirm_password");
 const rank = document.getElementById("rank");
 const platoon = document.getElementById("platoon");
 const active = document.getElementById("active");
-const elligibile = document.getElementById("elligible");
+const elligible = document.getElementById("elligibility");
 const form = document.getElementById("form");
 const errorElement = document.getElementById('error');
+const masterError = document.getElementById("master_error");
 
 
 let formListen = (event) => {
@@ -38,16 +39,18 @@ const setSuccess = (element) => {
     inputControl.classList.remove('error');
 }
 
-const setMasterError = () => {
-    const masterError = document.getElementById("master_error");
-
-    masterError.style.visibility = "visible";
+const setMasterError = (type) => {
+    if (type === 1) {
+        masterError.style.visibility = "visible";
+    } else {
+        masterError.style.visibility = "hidden";
+    }
 }
 
 function checkInputs() {
 
     // clear previous errors
-
+    setMasterError(0);
 
     // get values, remove white space from ends of inputs
     const memberV = member.value.trim();
@@ -58,37 +61,49 @@ function checkInputs() {
     const passwordV2 = password2.value.trim();
 
     try {
-        var rankV = rank.value.trim();
+        console.log(rank)
+        var rankV = rank.value;
+        console.log(rankV);
     }
     catch(err) {
         var rankV = '';
     }
 
     try {
-        var platoonV = platoon.value.trim();
+        console.log(platoon)
+        var platoonV = platoon.value;
+        console.log(platoonV);
     }
     catch(err) {
         var platoonV = '';
     }
 
     try {
-        var activeV = active.value.trim();
+        console.log(active)
+        var activeV = active.value;
+        console.log(activeV);
     }
     catch(err) {
         var activeV = '';
     }
+
     try {
-        var elligibleV = ellligible.value.trim();
+        console.log(elligible)
+        var elligibleV = elligible.value;
+        console.log(elligibleV);
     }
     catch(err) {
         var elligibleV = '';
     }
+
     var changes = 0;
+    var errors = 0;
 
 
     // ensure a user has been chosen
     if (memberV === "") {
         setError(member, 'User must be selected');
+        errors++;
     } else {
         setSuccess(member);
     }
@@ -118,6 +133,7 @@ function checkInputs() {
     if (passwordV != '') {
         if (passwordV.length < 8 || passwordV.length > 64) {
             setError(password, 'Password must be between 8 and 64 charactars long')
+            errors++;
         } else {
             setSuccess(password);
         }
@@ -127,38 +143,46 @@ function checkInputs() {
 
     if (passwordV.length > 0 && passwordV2.length == 0) {
         setError(password2, 'Please confirm your password');
+        errors++;
     } else if (passwordV2 !== passwordV) {
-        setError(password2, 'Passwords do not match')
-    } else if (passwordV === passwordV2 && passwordV.length > 0 && passwordV2.length > 0) {
+        setError(password2, 'Passwords do not match');
+        errors++;
+    } else if (passwordV === passwordV2 && passwordV.length > 0) {
         setSuccess(password2);
         changes++;
     } else {
         setSuccess(password2);
     }
 
+    console.log(rankV);
     if (rankV != '') {
         setSuccess(rank);
         changes++;
     }
 
+    console.log(platoonV);
     if (platoonV != '') {
         setSuccess(platoon);
         changes++;
     }
 
+    console.log(activeV);
     if (activeV != '') {
         setSuccess(active);
         changes++;
     }
 
+    console.log(elligibleV);
     if (elligibleV != '') {
         setSuccess(elligible);
         changes++;
     }
 
-    if (changes === 0) {
-        setMasterError();
-    } else {
+    if (changes === 0 && errors === 0) {
+        setMasterError(1);
+    } else if (errors > 0) {
+        setMasterError(0);
+    } else if (changes > 0 && errors === 0) {
         form.removeEventListener("submit", formListen);
         form.submit();
     }
